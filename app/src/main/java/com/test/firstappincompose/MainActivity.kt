5ff2b0c0693bf723284693c353a66e1ae8723178
Vipin <vignesh.raju@daimler.com>
2024-05-30 22:21:35 +0530

    state and stateless example

--- app/src/main/java/com/test/firstappincompose/MainActivity.kt
+++ app/src/main/java/com/test/firstappincompose/MainActivity.kt
@@ -3,19 +3,33 @@ package com.test.firstappincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -34,81 +48,57 @@ class MainActivity : ComponentActivity() {
             FirstAppInComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                       // DisplayOurText()
-                    var list = listOf("user1","user2","user3","user4")
-                   // sayHelloToUsers(list)
-                    ModifierTetDemo()
+                    var name by remember {
+                        mutableStateOf("")
+                    }
+//                    StatelessFun(name =  name) {
+//                        name = it
+//                    }
+                    Statefullfun()
                 }
             }
         }
     }
 }
 
+
+//StateFullExample
 @Composable
-fun ModifierTetDemo(){
-    Column (modifier = Modifier.background(Color.Blue)){
-        Text(
-            text = stringResource(id = R.string.txt_welcome_to_our_course),
-            modifier = Modifier.verticalScroll(state = rememberScrollState()).background(Color.Red))
+fun Statefullfun(){
+    val context = LocalContext.current
+    var isExpanded by remember {
+        mutableStateOf(false)
     }
 
-}
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)
+    ) {
+        Text(modifier = Modifier.weight(1f),
+            text = context.getString(R.string.txt_src),
+            maxLines = if(!isExpanded) 4 else Int.MAX_VALUE
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun modifierTextValue(){
-    FirstAppInComposeTheme {
-        ModifierTetDemo()
-    }
-}
-
-
-@Composable
-fun DisplayOurText(){
-    SelectionContainer {
-
-
-        Text(
-            text = "Hello friend \n welcome to our course",
-            color = Color.Blue,
-            fontSize = 22.sp,
-            style = TextStyle(textDecoration = TextDecoration.LineThrough),
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.ExtraBold,
-            fontFamily = FontFamily.Cursive,
-            maxLines = 2
-        )
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    FirstAppInComposeTheme {
-//        Column {
-//            DisplayOurText()
-//        }
-//
-//    }
-//}
-
-
-@Composable
-fun sayHelloToUsers(names : List<String>){
-    Column {
-        for(name in names){
-            Text(text = "Hello $name",
-                color=Color.Blue,
-                fontSize = 32.sp)
+        IconButton(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            onClick = { isExpanded = !isExpanded }) {
+            Image(imageVector = if (isExpanded) Icons.Filled.KeyboardArrowDown
+                else Icons.Filled.KeyboardArrowUp, contentDescription = "")
         }
     }
+}
 
+//stateLessExample
+@Composable
+fun StatelessFun(name : String, onNameChange : (String) ->Unit){
+Column(modifier = Modifier.padding(16.dp)) {
+    Text(text = "Hello $name",
+        modifier = Modifier.padding(bottom = 8.dp),
+        style = MaterialTheme.typography.headlineLarge)
+
+    OutlinedTextField(value = name,
+        onValueChange = onNameChange,
+        label = { Text(text = "Name")})
+
+}
 }
