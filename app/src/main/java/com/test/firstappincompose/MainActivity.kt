fe1963ac45807b8d18c7a51b74e7c02bcad999c8
Vipin <vignesh.raju@daimler.com>
2024-06-03 08:44:09 +0530

    checkbox example

--- app/src/main/java/com/test/firstappincompose/MainActivity.kt
+++ app/src/main/java/com/test/firstappincompose/MainActivity.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -17,11 +21,16 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -50,13 +59,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun  MyCheckBoxExample(){
     val context = LocalContext.current
-   Row(verticalAlignment = Alignment.CenterVertically) {
-       RadioButton(selected = false,
-           onClick = {
-               Toast.makeText(context,"showing message",Toast.LENGTH_SHORT).show()
-           })
-       Text(text = "Pizza")
+
+    val radioOptions = listOf("pizza","rajma","kaju kathri")
+
+    var (selectedItem,onOptionSelected) = remember {
+        mutableStateOf(radioOptions[0])
+    }
+
+    //Grouping the radio button in one group
+    Column(modifier = Modifier.selectableGroup()) {
+   radioOptions.forEach{label ->
+       Row(modifier = Modifier
+           .fillMaxWidth()
+           .height(56.dp)
+           .selectable(
+               selected = (selectedItem == label),
+               onClick = { onOptionSelected(label) },
+               role = Role.RadioButton
+           )
+           .padding(horizontal = 16.dp)
+       ) {
+               RadioButton(modifier = Modifier.padding(16.dp) ,
+                   onClick ={ onOptionSelected(label)},
+                   selected = (selectedItem==label)
+                   )
+           
+           Text(text = label)
+       }
    }
+    }
 
 }
 
